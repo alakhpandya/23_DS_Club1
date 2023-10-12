d6273a4d619215310c0fc85774a6fee7dacf051a
alakhpandya <alakhpandya@gmail.com>
2023-10-12 16:00:13 +0530

    oct12

---
+++ 03_Adv SQL/Oct10.sql
@@ -0,0 +1,10 @@
+select * from moviesdb.movies;
+
+use moviesdb;
+select * from languages;
+select * from actors;
+
+select title, imdb_rating, industry, studio
+from movies
+where industry = "bollywood";
+
