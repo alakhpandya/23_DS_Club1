f385f07fd8496ec6802729b5c30f0682d49b8e64
alakhpandya <alakhpandya@gmail.com>
2024-01-30 10:04:08 +0530

    Update jan30.sql

--- 03_Adv SQL/jan30.sql
+++ 03_Adv SQL/jan30.sql
@@ -25,3 +25,15 @@ select f.movie_id, m.title, f.budget
 from movies as m
 right join financials f
 on m.movie_id = f.movie_id;
+
+-- Get me the movie_id, title & budget sorted by their budget so that the movie with highest budget is on the top and lowest at the bottom.
+
+-- Print actor_id vs. movies names that actor has played a role in. Sort your result in the ascending order of actor_id
+
+-- Print number of movies played by each actor_id sorted in ascending order of actor_id
+
+-- Print number of movies played by each actor. That is, write name of the actor and number of movies played by them in against their names
+
+-- Print the movie titles, industry, budget, revenue & profit (profit = revenue - budget). Sort the result in descending order of profit. Clean the budget & revenue columns
+-- so that it becomes easy to compare the profit each movie did (Hint: If all of them are in the same currency and in the same unit (thousand/million/billion etc) then it is 
+-- possible to compare the profit of movies with each other)
